63857936c7bfbb15852ab95a9924c9f5eb13258e
ubuntu <djouaherrachid@gmail.com>
2025-09-30 14:17:43 +0200

    asm05

---
+++ asm05/asm05.s
@@ -0,0 +1,34 @@
+section .bss
+    buffer: resb 64
+
+section .text
+        global _start
+
+    _start:
+        mov rbx, [rsp+16]
+
+        xor rcx, rcx ;rcx is the str length
+
+        strlen_loop:
+        cmp byte[rbx+rcx], 0
+        je strlen_done
+        inc rcx
+        jmp strlen_loop
+
+        strlen_done:
+
+        ; Ajouter '\n' à la fin du buffer
+        mov byte [rbx+rcx], 10   ; ASCII 10 = '\n'
+        inc rcx                   ; longueur totale à écrire
+
+        ;write(stdout, *buffer, strlen)
+        mov rax, 1
+        mov rdi, 1
+        mov rsi, rbx
+        mov rdx, rcx
+        syscall
+
+        ;exit 0
+        mov rdi, 0
+        mov rax, 60
+        syscall
